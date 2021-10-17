0730c44e4c251c4b25cfba6a2a0e14988c2090af
mr-manson <60350677+mr-manson@users.noreply.github.com>
2021-10-17 18:23:13 +0300

    Add files via upload

---
+++ src/by/epamtc/nikalaichanka/task0301/minmax/MinMax.java
@@ -0,0 +1,27 @@
+package by.epamtc.nikalaichanka.task0301.minmax;
+
+public class MinMax {
+
+	public static int minFromMass(int[] mass) {
+		int min = mass[0];
+
+		for (int i = 0; i < mass.length; i++) {
+			if (mass[i] < min)
+				min = mass[i];
+		}
+
+		return min;
+	}
+
+	public static int maxFromMass(int[] mass) {
+		int max = mass[0];
+
+		for (int i = 0; i < mass.length; i++) {
+			if (mass[i] > max)
+				max = mass[i];
+		}
+
+		return max;
+	}
+
+}
